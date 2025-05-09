13565b45531d735476ee193478d3e3f9d3f2059c
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-05-09 22:11:17 +0200

    void

--- hello world.cpp
+++ hello world.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 // I miss python 
+void python(){
+    cout << endl;
+}
 
 int main() {
+    python();
     double y  = 1.0 / 3.0;
     cout << y << endl;
+    python();
     return 0;
 }
